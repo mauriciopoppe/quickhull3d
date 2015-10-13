0f0d4d682e7bb19a9b3a70b44e5db9dc2fd16668
Mauricio Poppe <mauricio.poppe@gmail.com>
2015-10-13 17:57:35 -0400

    Fix: standard

--- benchmark/pointGenerator.js
+++ benchmark/pointGenerator.js
@@ -1,26 +1,26 @@
 /**
  * Created by mauricio on 3/14/15.
  */
-var fs = require('fs');
-var limit = process.argv[2];
+var fs = require('fs')
+var limit = process.argv[2]
 
-var LIMIT = +limit;
-function p() {
-  return -LIMIT + Math.random() * 2 * LIMIT;
+var LIMIT = +limit
+function p () {
+  return -LIMIT + Math.random() * 2 * LIMIT
 }
 
-function genP() {
-  return [p(), p(), p()];
+function genP () {
+  return [p(), p(), p()]
 }
 
-var points = [];
+var points = []
 for (var i = 0; i < +limit; i += 1) {
-  points.push(genP());
+  points.push(genP())
 }
 
 fs.writeFile('./points' + limit + '.json', JSON.stringify(points), function (err) {
   if (err) {
-    throw err;
+    throw err
   }
   console.log('saved!')
-});
+})

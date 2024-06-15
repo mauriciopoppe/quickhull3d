7f368b7dc5cde9108ac95111ca4043872f429547
Mauricio Poppe <mauricio.poppe@gmail.com>
2024-06-15 19:44:51 -0400

    Update script to deploy webpage

--- deploy.cjs
+++ deploy.cjs
@@ -1,14 +1,18 @@
 const ghpages = require('gh-pages')
 const { execSync } = require('node:child_process')
 
-ghpages.publish('docs', {
-  nojekyll: true,
-  add: true,
-  async beforeAdd () {
-    execSync('npm run build')
-    execSync('cp dist/quickhull3d.js docs/')
+execSync('npm run build', { stdio: 'inherit' })
+execSync('cp dist/quickhull3d.js docs/', { stdio: 'inherit' })
+
+ghpages.publish(
+  'docs',
+  {
+    nojekyll: true,
+    add: true,
+    async beforeAdd() {}
+  },
+  function () {
+    execSync('rm docs/quickhull3d.js', { stdio: 'inherit' })
+    console.log('complete!')
   }
-}, function () {
-  execSync('rm docs/quickhull3d.js')
-  console.log('complete!')
-})
+)
